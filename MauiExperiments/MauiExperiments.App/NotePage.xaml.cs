d94cdf6c972ec4463722ef076c3bcf9b88192270
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-05-16 06:56:47 +0200

    NotePage added

---
+++ MauiExperiments/MauiExperiments.App/NotePage.xaml.cs
@@ -0,0 +1,33 @@
+namespace MauiExperiments.App;
+
+public partial class NotePage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePage()
+	{
+		InitializeComponent();
+
+        if (File.Exists(_fileName))
+        {
+            TextEditor.Text = File.ReadAllText(_fileName);
+        }        
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+
+        TextEditor.Text = string.Empty;
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+}
