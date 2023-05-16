f817541606e8bc2d583b8fc2710b3fd4a5180ea9
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-05-16 07:23:08 +0200

    Completed tutorial

---
+++ MauiExperiments/MauiExperiments.App/Models/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiExperiments.App.Models;
+
+internal class Note
+{
+    public string Filename { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+}
