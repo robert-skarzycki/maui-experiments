d50cdba20ec9be79a550fffe70fb5ac95232ba9e
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-05-30 06:14:33 +0200

    Rewrite for the latest steps

--- MauiExperiments/MauiExperiments.App/Views/NotePage.xaml.cs
+++ MauiExperiments/MauiExperiments.App/Views/NotePage.xaml.cs
@@ -1,56 +1,9 @@
 namespace MauiExperiments.App.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
     public NotePage()
-	{
-		InitializeComponent();
-
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
-    }
-
-    public string ItemId
     {
-        set { LoadNote(value); }
-    }
-
-    private void LoadNote(string fileName)
-    {
-        var noteModel = new Models.Note
-        {
-            Filename = fileName
-        };
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
-    }
-
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-        {
-            // Delete the file.
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-
-        await Shell.Current.GoToAsync("..");
+        InitializeComponent();
     }
 }
